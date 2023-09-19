9c78d317eb080fadc3b7cce90c9910a496138f78
tayouza <tayouzadev@gmail.com>
2023-09-18 23:39:11 -0300

    fix: mail layout | feat: add email contact

---
+++ resources/views/mails/contact-mail.blade.php
@@ -0,0 +1,6 @@
+<div>
+    Olá Taylor,<br>
+    {{ $name }} entrou em contato através do seu portifólio!<br>
+    Responda pelo email: {{ $email }}<br>
+    Ainda deixou a mensagem: {{ $contactMessage }}
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,19 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Mail\ContactMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('portfolio');
 })->name('portfolio');
+
+Route::post('/', function(Request $request) {
+    Mail::to(env('USER_EMAIL'))->send(new ContactMail($request));
+    return redirect('/');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
