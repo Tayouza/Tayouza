91548bdded898a6e1e4a230d6cc435fcfd7b8238
tayouza <tayouzadev@gmail.com>
2024-08-06 00:25:50 -0300

    fix: responsivity skills block in portifolio

--- resources/views/components/skills.blade.php
+++ resources/views/components/skills.blade.php
@@ -1,4 +1,4 @@
-<div {{$attributes->merge(['class' => 'bg-[#ffffff44] sm:w-auto w-full py-10 px-8 sm:px-10 text-center rounded-md
+<div {{$attributes->merge(['class' => 'bg-[#ffffff44] sm:w-auto w-full py-10 px-4 sm:px-10 text-center rounded-md
     duration-300
     hover:scale-110 hover:shadow-lg hover:shadow-[#00000033]'])}}>
     <h3 class="text-4xl mb-5">{{$titleBox}}</h3>
